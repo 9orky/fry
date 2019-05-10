dfadbb5552ca758ab821f3ee4220ff7089e35753
Katarzyna-Maryanska <kmaryanska@gmail.com>
2019-05-10 16:15:46 +0200

    base version

--- worker.js
+++ worker.js
@@ -1,4 +1,24 @@
-self.addEventListener('message', (event) => {
-    console.log(event.data);
-    self.postMessage({});
-});
+// import jsQR from 'jsQR';
+
+// console.log("1", 'function' === typeof importScripts)
+if( 'function' === typeof importScripts) {
+    importScripts("jsQR.js");
+    // addEventListener('message', onMessage);
+    // console.log(2)
+    addEventListener('message', (event) => {
+        const date = new Date();
+        try {
+            // console.log(event.data);
+            const { data, width, height } = event.data;
+            const start = date.getMilliseconds();
+            const code = jsQR(data, width, height);
+            console.log(start, date.getMilliseconds());
+            // console.log(height, width);
+            // console.log(code);
+            self.postMessage(code);
+            // console.log('OK')
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
