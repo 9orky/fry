efbef18e912b1fea6cb934cb5d3ef4d3efd4866f
9orky <tomszp@gmail.com>
2019-05-16 15:54:59 +0200

    add working camera scanner in firefox

--- src/Camera/qr.worker.js
+++ src/Camera/qr.worker.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-restricted-globals */
 import jsQR from 'jsqr';
 
-self.addEventListener('message', (event) => {
+addEventListener('message', (event) => {
     if (!event) {
         return;
     }
 
     try {
         const { data, width, height } = event.data;
-        const code = jsQR(data, width, height);
 
-        if (code) {
-            self.postMessage(code);
-        }
+        const code = jsQR(data, width, height, {
+            inversionAttempts: "dontInvert"
+        });
+        console.log(code, width, height);
     } catch (error) {
         console.log(error);
     }
